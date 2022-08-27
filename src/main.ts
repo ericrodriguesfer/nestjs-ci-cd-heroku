8bd8e8d7b8eb46a07b81168aa85f82a105c07e11
Eric Rodrigues <er601432@gmail.com>
2022-08-27 20:01:14 -0300

    fix: some changes for run

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
+
   await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
